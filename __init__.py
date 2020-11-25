b5f67fb9521023d0d8dec2c078c05a51c10c813b
PNikoui <57665675+PNikoui@users.noreply.github.com>
2020-11-25 19:44:55 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,33 @@
-from racetrack import racetrack
-from simulation import python_env
-from environment import RacecarEnv
+import logging 
+from gym.envs.registration import register  
 
-#from Racecar_v0.envs.environment import RacecarEnv
+logger = logging.getLogger(__name__)  
+
+register(   
+    id='racetrack-v0', 
+    entry_point='gym_Racecar.envs:racetrack',    
+#     timestep_limit=1000,
+#     turns = 5,
+    reward_threshold=8.0,
+    nondeterministic = True, 
+)
+
+register(     
+    id='simulation-v0',     
+    entry_point='gym_Racecar.envs:python_env',  
+#     timestep_limit=1000,
+#     turns = 5,
+    reward_threshold=10.0,    
+    nondeterministic = True, 
+)
+
+register(     
+    id='RacecarEnv-v0',     
+    entry_point='gym_Racecar.envs:RacecarEnv',    
+#     timestep_limit=1000,
+#     turns = 5,
+    reward_threshold=1.0,    
+    nondeterministic = True,
+)
+
+
