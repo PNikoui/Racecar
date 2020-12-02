e9c7bae540f66d4cabd0d4e9f6194b2613ed226f
PNikoui <57665675+PNikoui@users.noreply.github.com>
2020-12-02 17:36:47 +0100

    Add files via upload

--- New_model.py
+++ New_model.py
@@ -9,18 +9,21 @@ class seekndestroy(nn.Module):
         #deep
         self.Conv1 = nn.Conv1d(input_dim, 14, 5, stride = 5, padding =5)
         self.Pool1 = nn.MaxPool1d(2)
-        self.deep_1 = nn.Linear(30, 128, bias=True)
-        self.deep_2 = nn.Linear(128, 64, bias=True)
-        self.deep_3 = nn.Linear(64, 32, bias=True)
-        self.deep_4 = nn.Linear(32, 16, bias=True)
-        self.LSTM = nn.LSTM(input_size=16,
+        self.deep_1 = nn.Linear(31, 128, bias=True)
+        self.deep_2 = nn.Linear(128, 30, bias=True)
+        
+        self.LSTM = nn.LSTM(input_size=32,
                          hidden_size=50,
                          num_layers=1,
                          bidirectional=False)
         # Output layer 
         self.l_out = nn.Linear(in_features=50,
-                            out_features=output_dim,
+                            out_features=32,
                             bias=False)
+        
+        
+        self.deep_3 = nn.Linear(32, 32, bias=True)
+        self.deep_4 = nn.Linear(32, output_dim, bias=True)
         
         self.softmax = nn.LogSoftmax(dim=1)
 
@@ -35,6 +38,7 @@ class seekndestroy(nn.Module):
         # Convolution Block
         Sensor_Reading = inputs[:,:30]
         Input_Args = inputs[:,30:]   ## [dist/maxdist ratio to goal, angle, sign, POSITION, VELOCITY]
+        D_and_A_and_S = input_Args[:,:3]
         P_and_V = Input_Args[:,-2:]  ## Append P and V to output of convolution 
         xd = Sensor_Reading.unsqueeze(0).permute(0, 2, 1)
         xd = self.Conv1(xd)
@@ -45,19 +49,16 @@ class seekndestroy(nn.Module):
         # print(xd.shape)
         xd = xd.view(1,xd.shape[1]*xd.shape[2])
         # print(xd.shape)
-        xd = torch.cat((xd,P_and_V), 1)
         # print(xd.shape)
+        xd = torch.cat((xd,D_and_A_and_S), 1)
         xd = self.deep_1(xd)
         xd = self.relu(xd)
         xd = self.deep_2(xd)
         xd = self.relu(xd)
-        xd = self.deep_3(xd)
-        xd = self.relu(xd)
-        xd = self.deep_4(xd)
-        xd = self.relu(xd)
-        xd = xd.unsqueeze(0)
         
-        # LSTM Block
+        xd = torch.cat((xd,P_and_V), 1)
+        xd = xd.unsqueeze(0)
+          # LSTM Block
         # RNN returns output and last hidden state
         x, (h, c) = self.LSTM(xd)
         
@@ -65,8 +66,16 @@ class seekndestroy(nn.Module):
         x = x.view(-1, self.LSTM.hidden_size)
         
         # Output layer
-        x = self.l_out(x)
+        xd = self.l_out(x)
         
-        x = self.softmax(x)
+        xd = self.deep_3(xd)
+        xd = self.relu(xd)
+        xd = self.deep_4(xd)
+        xd = self.relu(xd)
+        
+      
+        
+#         x = self.softmax(x)
+        x = self.sigmoid(xd)
 
         return x
