b88621b226f8fcb9939a2731d58c22b2759a4d83
PNikoui <57665675+PNikoui@users.noreply.github.com>
2021-10-21 20:18:11 +0200

    File_Struct

--- Racecar/envs/__init__.py
+++
@@ -1,5 +0,0 @@
-from racetrack import racetrack
-from simulation import python_env
-from environment import RacecarEnv
-
-#from Racecar_v0.envs.environment import RacecarEnv
---
+++ gym-Racecar/envs/__init__.py
@@ -0,0 +1,3 @@
+from gym_Racecar.envs.racetrack import racetrack
+from gym_Racecar.envs.simulation import python_env
+from gym_Racecar.envs.environment import RacecarEnv
